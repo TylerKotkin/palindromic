1468f4831065f20855f80bc5cd1c1734767fb351
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-02 14:55:05 -0400

    recursion?

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,22 @@
 import re
 
-def is_palindrome(sentence):
+def is_palindrome(word):
     # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+    if main(True):
+        print("yes")
+    else:
+        print("no")
 
 
 def main():
     # TODO: put your input/output code here
     word = input("Please enter a word or sentence to test if it is palindromic: ")
-    newword = re.sub(r'[^A-Za-z]', "", word).lower()
-    newword == newword[::-1]
-    print(newword)
+    word2 = re.sub(r'[^A-Za-z]', "", word).lower()
+    word3 = word2[::-1]
+    if word2 == word3:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     main()
