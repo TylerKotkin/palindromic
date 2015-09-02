c6cb62dc79b06a2e9c2a0cada1f10fb4ac41456c
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-02 19:09:35 -0400

    working and passing test

--- palindrome.py
+++ palindrome.py
@@ -1,22 +1,33 @@
 import re
 
-def is_palindrome(word):
+
+def is_palindrome(sentence):
     # TODO: return True or False if the sentence is or isn't a palindrome
-    if main(True):
-        print("yes")
-    else:
-        print("no")
+
+    word2= re.sub(r'[^A-Za-z]', "", sentence).lower()
+
+    if len(word2) <= 1:
+        return True
+
+    elif word2[0] == word2[-1]:
+        return is_palindrome(word2[1:-1])
+    return False
 
 
 def main():
      # TODO: put your input/output code here
-    word = input("Please enter a word or sentence to test if it is palindromic: ")
-    word2 = re.sub(r'[^A-Za-z]', "", word).lower()
-    word3 = word2[::-1]
-    if word2 == word3:
-        print("y")
+    sentence = input("Please enter a word or sentence to test if it is palindromic: ")
+    #word2 = re.sub(r'[^A-Za-z]', "", sentence).lower()
+    if is_palindrome(sentence) == True:
+        print("That is a palindrome!")
     else:
-        print("n")
+        print("Sorry, that is not a palindrome.")
+    # word3 = word2[::-1]
+    # if word2 == word3:
+    #     print("y")
+    # else:
+    #     print("n")
+
 
 
 if __name__ == '__main__':
