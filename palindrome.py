2b4df7f5ad056e33c9c8919db789d6329a506eaf
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-02 16:44:14 -0400

    starting over

--- palindrome.py
+++ palindrome.py
@@ -2,34 +2,22 @@ import re
 
 def is_palindrome(word):
     # TODO: return True or False if the sentence is or isn't a palindrome
-    # word = input("Please enter a word or sentence to test if it is palindromic: ")
-    # word = main()
-    word2 = re.sub(r'[^A-Za-z]', "", word).lower()
-    word3 = word2[::-1]
-    if word2 == word3:
-    #     return True
-    # else:
-    #     return False
+    if main(True):
         print("yes")
     else:
-        print("No")
+        print("no")
 
 
 def main():
-#     # TODO: put your input/output code here
-#     word = input("Please enter a word or sentence to test if it is palindromic: ")
-#     return word
+     # TODO: put your input/output code here
+    word = input("Please enter a word or sentence to test if it is palindromic: ")
+    word2 = re.sub(r'[^A-Za-z]', "", word).lower()
+    word3 = word2[::-1]
+    if word2 == word3:
+        print("y")
+    else:
+        print("n")
 
-    # if is_palindrome() True:
-    #     print("yes")
-    # else:
-    #     print("no")
-    # word2 = re.sub(r'[^A-Za-z]', "", word).lower()
-    # word3 = word2[::-1]
-    # if word2 == word3:
-    #     return True
-    # else:
-    #     return False
 
 if __name__ == '__main__':
     main()
